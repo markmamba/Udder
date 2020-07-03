dfe510d211808123fa8421dfca9f359b5bbe9f8c
Mark Bautista <mark.hensonbautista@gmail.com>
2020-07-04 08:02:28 +0900

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Mark Bautista on 7/4/20.
+//  Copyright © 2020 Mark Bautista. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
